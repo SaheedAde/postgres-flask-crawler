0380bf161292535193cad7700cca11143c9fc6e7
Abiodun Adebowale <adebowalesaheedabiodun@gmail.com>
2022-09-28 01:32:05 +0200

    finished setup
    Signed-off-by: Abiodun Adebowale <adebowalesaheedabiodun@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # postgres-flask-crawler
+
+This is a simple Web Crawler App using Flask, Postgres, gunicorn and docker-compose.
+
+## Installation
+
+Step 0: Use [docker-compose](https://docs.docker.com/compose/) to install and start.
+```bash
+$ docker-compose build
+$ docker-compose up
+```
+
+## Usage
+
+Step 1: Initialise the database by hitting the endpoint
+```bash
+$ curl localhost:8088
+```
+
+Step 2: Crawl lucierne and store events in db
+```bash
+$ curl -X POST localhost:8088/crawl_events/
+```
+
+Step 3: Retrieve stored events
+```bash
+$ curl localhost:8088/get_events_in_db/
+```
--- project/db/access_database.py
+++ project/db/access_database.py
@@ -1,29 +1,35 @@
-from typing import Dict, List, Union
+from typing import Dict
 
 import psycopg2
 
 from project.db.config_database import ConfigDatabase
 
 class AccessDataBase(ConfigDatabase):
-
-    def __init__(self) -> None:
+    def __init__(self, drop: bool=False) -> None:
+        self.drop = drop
         self.logger.debug('Init Class AccessDataBase')
-        self.logger.debug(f'>>>>>>>>>>>>>>>>>>>>>>>> {self.postgres_access}')
         conn = psycopg2.connect(**self.postgres_access)
         cursor = conn.cursor()
+        if self.drop:
+            cursor.execute(f'DROP TABLE IF EXISTS {self.table_name};')
+
         query = f'''CREATE TABLE IF NOT EXISTS {self.table_name}(
-                    message_id SERIAL PRIMARY KEY NOT NULL,
-                    message_title varchar(30) NOT NULL UNIQUE,
-                    author_name varchar(30) NOT NULL,
-                    message_text varchar(200) NOT NULL,
-                    creation_date date NOT NULL)'''
+                    event_id SERIAL PRIMARY KEY NOT NULL,
+                    event_title varchar(200) NOT NULL,
+                    artists varchar(30)[] NOT NULL,
+                    starttime date NOT NULL,
+                    venue varchar(30) NOT NULL,
+                    works varchar(30)[] NOT NULL,
+                    image_link varchar(200) NOT NULL,
+                    starting_price integer NOT NULL,
+                    creation_date date DEFAULT CURRENT_TIMESTAMP)'''
         cursor.execute(query)
         cursor.close()
         conn.commit()
         self.logger.debug('CLASS AccessDataBase INITED')
 
 
-    def get_messages(self, indice: int=0):
+    def get_events(self, indice: int=0):
         self.logger.debug('GETTING DATAS')
         conn = psycopg2.connect(**self.postgres_access)
         self.logger.debug('DB CONNECTED')
@@ -39,76 +45,23 @@ class AccessDataBase(ConfigDatabase):
             end = indice*3-1; start = end-2
             return datas[start:end+1]
         else: return datas
-    
 
-    def get_message_by_condition(self, target_query: List[Union[int, str]]=[]):
-        self.logger.debug('GETTING DATA')
-        conn = psycopg2.connect(**self.postgres_access)
-        self.logger.debug('DB CONNECTED')
-        cursor = conn.cursor()
-        select_query = f"select * from {self.table_name} where {target_query[0]} = '{target_query[1]}';"
-        cursor.execute(select_query)
-        self.logger.debug('QUERY EXECUTED')
-        data = cursor.fetchone()
-        cursor.close()
-        conn.commit()
-        self.logger.debug('RETURNING DATA')
-        return data
-    
-    
-    def insert_message(self, message_rows: Dict[int, str]):
-        self.logger.debug(f'INSERT INTO {self.table_name} VALUES({message_rows}) ')
+
+    def insert_event_in_db(self, data_rows: Dict[int, str]):
+        self.logger.debug(f'INSERT INTO {self.table_name} VALUES({data_rows}) ')
         conn = psycopg2.connect(**self.postgres_access)
         cursor = conn.cursor()
         select_query = f"INSERT INTO {self.table_name}" \
-                        "(message_title, author_name, message_text, creation_date)" \
-                        "VALUES(%s, %s, %s, %s);"
-        insert_tuple = (message_rows['message_title'],
-                        message_rows['author_name'],
-                        message_rows['message_text'],
-                        message_rows['creation_date'])
+                        "(event_title, artists, starttime, venue, works, image_link, starting_price)" \
+                        "VALUES(%s, %s, %s, %s, %s, %s, %s);"
+        insert_tuple = (data_rows['title'],
+                        data_rows['artists'],
+                        data_rows['datetime'],
+                        data_rows['venue'],
+                        data_rows['works'],
+                        data_rows['image_link'],
+                        data_rows['starting_price'])
         cursor.execute(select_query, insert_tuple)
         cursor.close()
         conn.commit()
         self.logger.debug('SUCCESS')
-    
-    
-    def update_message(self, message_id, args: list):
-        self.logger.debug('UPDATE DATAS')
-        conn = psycopg2.connect(**self.postgres_access)
-        self.logger.debug('DB CONNECTED')
-        cursor = conn.cursor()
-        select_query = f"update {self.table_name} set {args[0]} = '{args[1]}' where message_id = {message_id};"            
-        cursor.execute(select_query)
-        self.logger.debug('QUERY EXECUTED')
-        cursor.close()
-        conn.commit()
-        
-    
-    def remove_message(self, message_id):
-        self.logger.debug('REMOVING DATA')
-        conn = psycopg2.connect(**self.postgres_access)
-        self.logger.debug('DB CONNECTED')
-        cursor = conn.cursor()
-        select_query = f'delete from {self.table_name} where message_id = {message_id};'
-        cursor.execute(select_query)
-        self.logger.debug('DELETE EXECUTED')
-        cursor.close()
-        conn.commit()
-        
-    
-    def get_message_length(self):
-        self.logger.debug('GETTING DATA')
-        conn = psycopg2.connect(**self.postgres_access)
-        self.logger.debug('DB CONNECTED')
-        cursor = conn.cursor()
-        select_query = f"select count(*) from {self.table_name};"
-        cursor.execute(select_query)
-        self.logger.debug('QUERY EXECUTED')
-        data = cursor.fetchone()[0]
-        print(data)
-        cursor.close()
-        conn.commit()
-        self.logger.debug('RETURNING DATA')
-        return data
-
